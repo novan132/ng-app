1c12cd36a07fb42e6bc0f3a0ae74341df954e8cf
Novan <l4novan132@gmail.com>
2023-07-29 14:15:59 +0700

    create message-details comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirstCompComponent } from './first-comp/first-comp.component';
+import { MessageDetailsComponent } from './message-details/message-details.component';
 
 @NgModule({
-  declarations: [AppComponent, FirstCompComponent],
+  declarations: [AppComponent, FirstCompComponent, MessageDetailsComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
