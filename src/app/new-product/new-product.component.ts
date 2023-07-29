eed80a382e09ad4b6b6500f9a3de3721669f81c0
Novan <l4novan132@gmail.com>
2023-07-29 17:53:02 +0700

    add functionality to create product

--- src/app/new-product/new-product.component.ts
+++ src/app/new-product/new-product.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProductRepresentation } from '../services/api/models/product-representation';
+import { ProductService } from '../services/api/products/product.service';
 
 @Component({
   selector: 'app-new-product',
   templateUrl: './new-product.component.html',
-  styleUrls: ['./new-product.component.scss']
+  styleUrls: ['./new-product.component.scss'],
 })
 export class NewProductComponent {
+  product: ProductRepresentation = {};
 
+  constructor(private service: ProductService, private router: Router) {}
+
+  saveProduct() {
+    this.service.createProduct(this.product).subscribe({
+      next: (data) => {
+        this.router.navigate(['products']);
+      },
+    });
+  }
 }
