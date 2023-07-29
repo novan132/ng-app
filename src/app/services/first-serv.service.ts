e8353f0b049714cdeb91e6c0fc8a5eae54cf86b7
Novan <l4novan132@gmail.com>
2023-07-29 15:00:20 +0700

    implement message service

--- src/app/services/first-serv.service.ts
+++ src/app/services/first-serv.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FirstServService {
+  messages: Array<any> = [];
 
-  constructor() { }
+  constructor() {
+    this.init();
+  }
+
+  init(): void {
+    this.insert({
+      name: 'novan',
+      email: 'novan@mail.com',
+      message: 'hello',
+    });
+    this.insert({
+      name: 'john',
+      email: 'john@mail.com',
+      message: 'hello john',
+    });
+    this.insert({
+      name: 'jane',
+      email: 'jane@mail.com',
+      message: 'hello jane',
+    });
+  }
+
+  insert(message: { name: string; email: string; message: string }): void {
+    this.messages.push(message);
+  }
+
+  getAllMessages(): any[] {
+    return this.messages;
+  }
+
+  deleteMessage(index: number): void {
+    this.messages.splice(index, 1);
+  }
 }
