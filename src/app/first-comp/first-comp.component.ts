1830f839813c99261c528f9692a092665d7c41f4
Novan <l4novan132@gmail.com>
2023-07-29 14:14:07 +0700

    implement component and add 2-way binding

--- src/app/first-comp/first-comp.component.ts
+++ src/app/first-comp/first-comp.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-first-comp',
   templateUrl: './first-comp.component.html',
-  styleUrls: ['./first-comp.component.scss']
+  styleUrls: ['./first-comp.component.scss'],
 })
 export class FirstCompComponent {
+  name: string = '';
+  email: string = '';
+  message: string = '';
+  isSubmitted: boolean = false;
+  messages: Array<any> = [];
 
+  onSubmit() {
+    this.isSubmitted = true;
+    this.messages.push({
+      name: this.name,
+      email: this.email,
+      message: this.message,
+    });
+    console.log(this.messages);
+  }
 }
